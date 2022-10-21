00560dda2f99189aaff7c8fa9c2a96f624f520d8
Marouane Qaos <mqaos@e1r4p14.1337.ma>
2022-10-21 22:21:59 +0100

    add new srlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,12 +6,11 @@
 /*   By: mqaos <mqaos@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 12:51:39 by mqaos             #+#    #+#             */
-/*   Updated: 2022/10/21 21:13:57 by mqaos            ###   ########.fr       */
+/*   Updated: 2022/10/21 22:17:10 by mqaos            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
 
 size_t	ft_strlcat(char	*dst, const char	*src, size_t n)
 {
@@ -20,29 +19,30 @@ size_t	ft_strlcat(char	*dst, const char	*src, size_t n)
 	size_t			d;
 
 	i = 0;
-	d = strlen(dst);
-	s = strlen(src);
+	s = ft_strlen(src);
+	if (dst == NULL && n == 0)
+		return (s);
+	d = ft_strlen(dst);
 	if (d > n)
 		return (n + s);
-	while (n > 0 && src[i] != '\0')
+	while (d + 1 < n && src[i] != '\0')
 	{
-		dst[n] = src[i];
-		n++;
+		dst[d] = src[i];
+		d++;
 		i++;
 	}
-	dst[i] = '\0';
-	return (d + i);
+	dst[d] = '\0';
+	return (ft_strlen(dst) + ft_strlen(src + i));
 }
+
 
 // int main(int argc, char const *argv[])
 // {
 // 	char *str = "n\0AA";
-// 	char buff1[0xF00] = "AAAAAAAAAAAAAAAA";
-// 	char buff2[0xF00] = "AAAAAAAAAAAAAAAA";
-// 	char buff3[0xF00] = "AAAAAAAAAAAAAAAA";
-// 	char buff4[0xF00] = "AAAAAAAAAAAAAAAA";
-// 	size_t max = 10;
-//     printf("%lu\n",strlcat(buff1,buff2,max));
-//     printf("%lu",ft_strlcat(buff3,buff4,max));
+//     char b[0xF] = "nyan !";
+// 	size_t max = 0;
+	
+//     printf("%lu\n",strlcat(((void *)0), b, 0));
+//     printf("%lu",ft_strlcat(((void *)0), b, 0));
 //     return 0;
 // }
